e9432d6793bcd2ef1eb80ec3007c5ca87ec34e4c
CameronCodingProjects <cameron2733@gmail.com>
2022-02-21 13:31:53 -0500

    Contant File
    Add a constant File

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -16,7 +16,7 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = "⚡️FlashChat"
+        titleLabel.text = K.appName
         
         
 // Comments way of doing it without cocoa pods
